49d96b9ca4039f560401ac977271701724524898
Jason Chavannes <jason.chavannes@gmail.com>
2017-01-23 00:06:07 -0800

    Allow just templates or just public

--- web/Server.go
+++ web/Server.go
@@ -16,29 +16,32 @@ type Server struct {
 }
 
 func (s *Server) Run() {
-	s.Routes = append(s.Routes, Route{
-		Pattern: "/",
-		Handler: func(r *Request) {
-			renderer, err := GetRenderer(s.TemplateDirectory)
-			check(err)
+	// Templates
+	if len(s.TemplateDirectory) > 0 {
+		s.Routes = append(s.Routes, Route{
+			Pattern: "/",
+			Handler: func(r *Request) {
+				renderer, err := GetRenderer(s.TemplateDirectory)
+				check(err)
 
-			writer := *r.HttpResponseWriter
-			writer.Header().Set("Content-Type", "text/html")
+				writer := *r.HttpResponseWriter
+				writer.Header().Set("Content-Type", "text/html")
 
-			filename := GetFilenameFromRequest(r.HttpRequest)
+				filename := GetFilenameFromRequest(r.HttpRequest)
 
-			if len(filename) == 0 {
-				filename = "index"
-			}
+				if len(filename) == 0 {
+					filename = "index"
+				}
 
-			renderer.Render([]string{
-				filename + ".html",
-				"404.html",
-			}, *r.HttpResponseWriter, nil)
-		},
-	})
+				renderer.Render([]string{
+					filename + ".html",
+					"404.html",
+				}, *r.HttpResponseWriter, nil)
+			},
+		})
+	}
 
-	// Requests
+	// Routes
 	s.HttpServerMux = http.NewServeMux()
 	for _, route := range s.Routes {
 		fmt.Printf("Setting pattern: %s\n", route.Pattern)
@@ -53,9 +56,11 @@ func (s *Server) Run() {
 	}
 
 	// Static assets
-	s.FileHandler = http.FileServer(http.Dir(s.StaticDirectory))
-	staticDirectory := "/" + s.StaticDirectory + "/"
-	s.HttpServerMux.Handle(staticDirectory, http.StripPrefix(staticDirectory, s.FileHandler))
+	if len(s.StaticDirectory) > 0 {
+		s.FileHandler = http.FileServer(http.Dir(s.StaticDirectory))
+		staticDirectory := "/" + s.StaticDirectory + "/"
+		s.HttpServerMux.Handle(staticDirectory, http.StripPrefix(staticDirectory, s.FileHandler))
+	}
 
 	err := http.ListenAndServe(":" + strconv.Itoa(s.Port), s.HttpServerMux)
 	check(err)
