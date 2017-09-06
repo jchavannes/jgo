3bd4570eb7d31cc22d5dd6632d3939700055713a
Jason Chavannes <jason.chavannes@gmail.com>
2017-09-05 23:47:58 -0700

    Test don't log

--- jlog/jlog_test.go
+++ jlog/jlog_test.go
@@ -19,8 +19,26 @@ func TestLog(t *testing.T) {
 	message := "test message"
 	logger.Debug(message)
 	logged := writer.String()
-	fmt.Printf("message: %s, logged: %s\n", message, logged)
+	fmt.Printf("TestLog -\n message: %s, logged: %s\n", message, logged)
 	if logged != message {
 		t.Errorf("logged message does not match")
+	}
+}
+
+func TestDontLog(t *testing.T) {
+	writer := new(bytes.Buffer)
+	var logger jlog.Logger
+	logger.SetLogWriter(jlog.LogWriter{
+		Writer: writer,
+		Levels: []jlog.LogLevel{
+			jlog.DEBUG,
+		},
+	})
+	message := "test message"
+	logger.Trace(message)
+	logged := writer.String()
+	fmt.Printf("TestDontLog -\n message: %s, logged: %s\n", message, logged)
+	if logged != "" {
+		t.Errorf("logged trace message when log level only set to debug")
 	}
 }
