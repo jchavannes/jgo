c659389fc2ba298d944500cabbb925d2b9e9b703
Jason Chavannes <jason.chavannes@gmail.com>
2020-04-15 17:37:03 -0700

    String helper functions

--- jutil/string.go
+++ jutil/string.go
@@ -16,3 +16,24 @@ func UnescapeByteString(s string) []byte {
 	s, _ = strconv.Unquote(`"` + s + `"`)
 	return []byte(s)
 }
+
+func StringInSlice(needle string, haystack []string) bool {
+	for _, item := range haystack {
+		if item == needle {
+			return true
+		}
+	}
+	return false
+}
+
+func RemoveDupeStrings(strings []string) []string {
+	for i := 0; i < len(strings); i++ {
+		for g := i + 1; g < len(strings); g++ {
+			if strings[i] == strings[g] {
+				strings = append(strings[:g], strings[g+1:]...)
+				g--
+			}
+		}
+	}
+	return strings
+}
