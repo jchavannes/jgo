1f42e8dcaaf80d6c96953d13279ec5d5828fac72
Jason Chavannes <jason.chavannes@gmail.com>
2022-09-10 23:06:32 -0700

    Reverse string

--- jutil/string.go
+++ jutil/string.go
@@ -6,6 +6,14 @@ import (
 	"unicode/utf8"
 )
 
+func ReverseString(s string) string {
+	r := []rune(s)
+	for i, j := 0, len(r)-1; i < len(r)/2; i, j = i+1, j-1 {
+		r[i], r[j] = r[j], r[i]
+	}
+	return string(r)
+}
+
 func ReverseStringSlice(slice []string) []string {
 	last := len(slice) - 1
 	for i := 0; i < len(slice)/2; i++ {
