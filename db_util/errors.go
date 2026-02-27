8a5b76ee322da9993a9963fc18a79835c94d2381
Jason Chavannes <jason.chavannes@gmail.com>
2026-02-26 20:30:28 -0800

    More DB connection errors

--- db_util/errors.go
+++ db_util/errors.go
@@ -2,17 +2,21 @@ package db_util
 
 import (
 	"errors"
+
 	"github.com/jchavannes/jgo/jerr"
 )
 
 const (
-	DuplicateEntryErrorMessage    = "Duplicate entry"
-	InvalidConnectionErrorMessage = "invalid connection"
-	NoRowsInResultSetErrorMessage = "sql: no rows in result set"
-	DatabaseClosedErrorMessage    = "sql: database is closed"
-	RecordNotFoundErrorMessage    = "record not found"
-	LockTimeoutErrorMessage       = "Error 1205: Lock wait timeout exceeded; try restarting transaction"
-	TableDoesntExistErrorMessage  = "Error 1146: Table '"
+	DuplicateEntryErrorMessage     = "Duplicate entry"
+	InvalidConnectionErrorMessage  = "invalid connection"
+	BadConnectionErrorMessage      = "driver: bad connection"
+	NoRowsInResultSetErrorMessage  = "sql: no rows in result set"
+	DatabaseClosedErrorMessage     = "sql: database is closed"
+	RecordNotFoundErrorMessage     = "record not found"
+	LockTimeoutErrorMessage        = "Error 1205: Lock wait timeout exceeded; try restarting transaction"
+	TooManyConnectionsErrorMessage = "Error 1040: Too many connections"
+	ServerShutdownErrorMessage     = "Error 1053: Server shutdown in progress"
+	TableDoesntExistErrorMessage   = "Error 1146: Table '"
 )
 
 var (
@@ -29,7 +33,8 @@ func IsRecordNotFoundError(e error) bool {
 }
 
 func IsConnectionError(e error) bool {
-	return IsLockTimeoutError(e) || IsInvalidConnectionError(e) || IsDatabaseClosedError(e)
+	return IsLockTimeoutError(e) || IsInvalidConnectionError(e) || IsDatabaseClosedError(e) ||
+		IsTooManyConnectionsError(e) || IsServerShutdownError(e) || IsBadConnectionError(e)
 }
 
 func IsLockTimeoutError(e error) bool {
@@ -46,6 +51,18 @@ func IsNoRowsInResultSetError(e error) bool {
 
 func IsDatabaseClosedError(e error) bool {
 	return jerr.HasError(e, DatabaseClosedErrorMessage)
+}
+
+func IsTooManyConnectionsError(e error) bool {
+	return jerr.HasError(e, TooManyConnectionsErrorMessage)
+}
+
+func IsServerShutdownError(e error) bool {
+	return jerr.HasError(e, ServerShutdownErrorMessage)
+}
+
+func IsBadConnectionError(e error) bool {
+	return jerr.HasError(e, BadConnectionErrorMessage)
 }
 
 func IsTableDoesntExistError(e error) bool {
